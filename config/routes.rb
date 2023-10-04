5aa510723014e1b0065127efd153f5cb0ed8b543
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2023-10-04 04:02:05 -0300

    Configura rotas que serão usadas no frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
       resources :tasks
     end
   end
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  get "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
