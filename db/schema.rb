cbc361c1e3419e2cc5d7c5af1080ec81cf18275a
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-02-11 22:39:29 -0300

    Cria migration pra user corretamente

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_095508) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_130158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "description", null: false
-    t.date "date", null: false
-    t.boolean "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest", null: false
   end
 
-  add_foreign_key "tasks", "users"
 end
