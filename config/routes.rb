2751afa933e67e7c750658856a92ba8af8f76eee
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-02-11 22:40:21 -0300

    Ajusta signup para post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   post "refresh", controller: :refresh, action: :create
   post "signin", controller: :signin, action: :create
-  get "signup", controller: :signup, action: :create
+  post "signup", controller: :signup, action: :create
   delete "signin", controller: :signin, action: :destroy
 
 
