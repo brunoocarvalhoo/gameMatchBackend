8fb58d5a32c1b55910b53b2ea75b4b5763c76b7e
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-02-18 02:43:01 -0300

    Altera rota do logout para get

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post "refresh", controller: :refresh, action: :create
   post "signin", controller: :signin, action: :create
   post "signup", controller: :signup, action: :create
-  delete "signin", controller: :signin, action: :destroy
+  get "signin", controller: :signin, action: :destroy
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
